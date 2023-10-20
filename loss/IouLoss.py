a494f84bb7953e25911c9220a6e80a99083d10b9
qianledan <45925696+qianledan@users.noreply.github.com>
2023-10-20 14:33:57 +0800

    Create IouLoss.py

---
+++ loss/IouLoss.py
@@ -0,0 +1,42 @@
+class IoULoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input, target):
+        smooth = 1e-5
+        input = torch.sigmoid(input)
+        num = target.size(0)
+        input = input.view(num, -1)
+        target = target.view(num, -1)
+        intersection = (input * target)
+        
+        iou = (intersection.sum(1) + smooth) / (input.sum(1) + target.sum(1) - intersection.sum(1) + smooth)
+        iou_loss = 1 - iou.sum() / num
+        
+        return iou_loss    
+
+#multi classfication IoU Loss
+class SoftIoULoss(nn.Module):
+    def __init__(self, n_classes):
+        super(SoftIoULoss, self).__init__()
+        self.n_classes = n_classes
+
+    @staticmethod
+    def to_one_hot(tensor, n_classes):
+        n, h, w = tensor.size()
+        one_hot = torch.zeros(n, n_classes, h, w).scatter_(1, tensor.view(n, 1, h, w), 1)
+        return one_hot
+
+    def forward(self, input, target):
+        # logit => N x Classes x H x W
+        # target => N x H x W
+
+        N = len(input)
+
+        pred = F.softmax(input, dim=1)
+        target_onehot = self.to_one_hot(target, self.n_classes)
+
+        # Numerator Product
+        inter = pred * target_onehot
+        # Sum over all pixels N x C x H x W => N x C
+        inter = inter.view(N, self.n_classes, -1).sum(2)
